0ed2548e33c794d41a660c9cfbedc3b1e92408e1
Mario Solano <marioqwe@vt.edu>
2019-01-27 15:24:40 -0500

    access log format

--- server/bin/run.sh
+++ server/bin/run.sh
@@ -13,7 +13,10 @@ if [ "$DJANGO_ENV" = 'production' ]; then
     gunicorn app.config.wsgi:application \
              --bind 0.0.0.0:8000 \
              --workers 3 \
-             --log-level=warning
+             --log-level=warning \
+             --access-logfile "-" \
+             --access-logformat  "{'remote_ip':'%(h)s','request_id':'%({X-Request-Id}i)s','response_code':'%(s)s','request_method':'%(m)s','request_path':'%(U)s','request_querystring':'%(q)s','request_timetaken':'%(D)s','response_length':'%(B)s'}"
+
 else # default to development.
     echo "Running Development Server"
     python manage.py runserver 0.0.0.0:8000
