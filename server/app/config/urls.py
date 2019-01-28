61c5d9e299be189e0a92888c0b75f1c152ac8693
Mario Solano <marioqwe@vt.edu>
2019-01-28 15:22:12 -0500

    add django admin

--- server/app/config/urls.py
+++ server/app/config/urls.py
@@ -1,9 +1,14 @@
+from django.contrib import admin
 from django.conf.urls import url
 
 from ..articles import views
 
 
 urlpatterns = [
+    # REST API
     url(r'^v1/articles/', views.articles, name='articles'),
     url(r'^v1/sources/', views.sources, name='articles-sources'),
+
+    # Django Admin
+    url(r'^admin/', admin.site.urls),
 ]
