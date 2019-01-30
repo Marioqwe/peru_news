b4540f235618f6dfc103a8c0b73502ad9b0c89ac
Mario Solano <marioqwe@vt.edu>
2019-01-29 22:26:57 -0500

    quotes

--- client/webpack/dev.js
+++ client/webpack/dev.js
@@ -1,4 +1,4 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
