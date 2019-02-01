2a0ae364f7c6fa5f8f248351f1926af5f71d879a
Mario Solano <marioqwe@vt.edu>
2019-02-01 17:09:22 -0500

    simplify scrapper because we only care about headlines

--- scrappy/scrappers/rpp.py
+++ scrappy/scrappers/rpp.py
@@ -1,9 +1,5 @@
-import json
-import traceback
-from html import unescape
-
 from scrappy import settings
-from scrappy.db import RedisManager
+from scrappy.logger import Logger
 from scrappy.scrappers.base import Scrapper
 from scrappy.utils import make_soup
 
@@ -20,97 +16,35 @@ class RPP(Scrapper):
     SECTIONS = ('politica', 'mundo', 'economia', 'actualidad', 'deportes',
                 'entretenimiento', 'tecnologia', 'ciencia')
 
-    def _scrap_article(self, loc, section):
-        target_url = self.BASE_URL + loc
-        self.logger.info('|---\t%s', target_url)
-
-        prev_failed = RedisManager.get(target_url)
-        if prev_failed is not None:
-            if prev_failed != 'True':
-                self.logger.info('|    \t* Success - already in db.')
-                data = json.loads(prev_failed)
-                return data
-
-            if not RETRY_FAILED_URLS:
-                self.logger.info('|    \t* Failure - already in db.')
-                return
-
-        soup = make_soup(target_url)
-        tags = soup.find_all('script', {'type': 'application/ld+json'})
-        if len(tags) != 1:
-            self.logger.info('|    \t* Failure - bad html')
-            RedisManager.save(target_url, True)  # set as prev failed.
-            return
-
-        try:
-            # reason for 'strict' in stack overflow's 9295439.
-            obj = json.loads(tags[0].string, strict=False)
-        except json.JSONDecodeError:
-            self.logger.info('|    \t* Failure - bad json in html')
-            RedisManager.save(target_url, True)
-            return
-
-        try:
-            ac = {  # article content
-                'source': {'id': self.id_, 'name': self.name},
-                'author': unescape(obj['author']),
-                'headline': unescape(obj['alternativeHeadline']),
-                'summary': unescape(obj['description']),
-                'body': unescape(obj['articleBody']).replace('&nbsp;', ' '),
-                'section': section,
-                'urlToImage': unescape(obj['image']['Url']),
-                'url': target_url,
-                'publishedAt': unescape(obj['datePublished']),
-            }
-
-            if len(ac['body']) > 5000:
-                self.logger.info('|    \t* Failure - json is too big in html')
-                RedisManager.save(target_url, True)
-                return
-
-            RedisManager.save(target_url, json.dumps(ac).encode())
-            self.logger.info('|    \t* Success - saved to db.')
-            return ac
-        except KeyError:
-            self.logger.info('|    \t* Failure - bad json key in html\n%s', traceback.format_exc())
-            RedisManager.save(target_url, True)
-            return
-        except Exception:
-            self.logger.critical(traceback.format_exc())
-            RedisManager.save(target_url, True)
-            return
-
-    def _get_section_generator(self, url, section):
-        self.logger.info('Scrapping %s', section)
-
-        soup = make_soup(url)
-        tags = soup.find_all('div', {'class': 'story'})
-        if len(tags) != 1:
-            self.logger.info('Error: bad html [%s]', url)
-            return
-
-        tags = tags[0].find_all('div', {'class': 'col-primary'})
-        if len(tags) != 1:
-            self.logger.info('Error: bad html [%s]', url)
-            return
-
-        a_list = tags[0].find_all('article')
-        for a in a_list:
-            try:
-                a_loc = a.figure.a.get('href')
-                a_obj = self._scrap_article(a_loc, section)
-                yield a_obj
-            except Exception:
-                self.logger.error('Error scrapping [%s]\n%s', url, traceback.format_exc())
-                continue
-
     def _scrap_section(self, section, date):
         url = '%s/archivo/%s/%s/' % (self.BASE_URL, section, date)
-        generator = self._get_section_generator(url, section)
+        soup = make_soup(url)
+        res_set = soup.find_all('div', {'class': 'col-primary'})
+        if len(res_set) != 1:
+            return
+        res = res_set[0]
+        articles = res.find_all('article')
         data = []
-        if generator is not None:
-            # ac (article content) refers to the scrapped article's data.
-            for ac in generator:
-                if ac is not None:
-                    data.append(ac)
+        for article in articles:
+            h3 = article.h3
+            time = article.time
+            if h3 and time:
+                title = h3.a.get_text()
+                url = self.BASE_URL + h3.a.get('href')
+                date = time.get('data-x')
+
+                data.append({
+                    'source': {'id': self.id_, 'name': self.name},
+                    'headline': title,
+                    'section': section,
+                    'url': url,
+                    'publisedAt': date,
+                })
+
         return data
+
+
+if __name__ == '__main__':
+    logger = Logger()
+    scrapper = RPP(logger)
+    scrapper.run(section='politica', test=True)
